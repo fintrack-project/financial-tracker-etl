d258f13eb5496e4fba41fa22f2dab0ff1321a193
johnkim1216 <johnmjkim1216@gmail.com>
2025-04-08 13:23:45 +1000

    fixed correct name

--- etl/jobs/fetch_market_average_data/__init__.py
+++ etl/jobs/fetch_market_average_data/__init__.py
@@ -2,4 +2,4 @@
 Job for fetching live market average data (SPY and QQQ) and saving it to the database.
 """
 
-from .fetch_live_market_average_data import run
+from .fetch_market_average_data import run
